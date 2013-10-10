874d8d6738bcadcbd0a696bfcb8bb117745c0273
Alexander Dymo <adymo@eligoenergy.com>
2013-10-10 16:57:37 -0500

    Set deps

--- kdevelop-php-docs.rb
+++ kdevelop-php-docs.rb
@@ -5,5 +5,6 @@ class KdevelopPhpDocs < BaseKdeFormula
   url 'http://download.kde.org/stable/kdevelop/4.5.1/src/kdevelop-php-docs-1.5.1.tar.bz2'
   sha1 '9bdb654373970f46d01359fdcae6d879c405760a'
 
+  depends_on 'kdevelop-php'
   kde_build_deps
 end
--- kdevelop-php.rb
+++ kdevelop-php.rb
@@ -5,5 +5,6 @@ class KdevelopPhp < BaseKdeFormula
   url 'http://download.kde.org/stable/kdevelop/4.5.1/src/kdevelop-php-1.5.1.tar.bz2'
   sha1 'c047724c255417b361d3ce1898555c2b9183f5b2'
 
+  depends_on 'kdevelop'
   kde_build_deps
 end
