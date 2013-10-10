7f8afaac1f660146dcf0ec2b6f9ead0088389c0f
Alexander Dymo <adymo@eligoenergy.com>
2013-10-10 17:20:26 -0500

    KDevelop-PG-Qt tarball is broken and empty, so use git

--- kdevelop-pg-qt.rb
+++ kdevelop-pg-qt.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 
 class KdevelopPgQt < BaseKdeFormula
   homepage 'http://kdevelop.org/'
-  url 'http://anongit.kde.org/kdevelop-pg-qt/kdevelop-pg-qt-latest.tar.gz'
+  head 'http://anongit.kde.org/kdevelop-pg-qt', using: :git
   version '1.0'
 
   depends_on 'kdelibs'
