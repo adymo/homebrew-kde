5e09e197d8deabb004818e5640812b34efe6f079
Alexander Dymo <adymo@eligoenergy.com>
2013-10-10 16:55:36 -0500

    Php language support for KDevelop

---
+++ kdevelop-php-docs.rb
@@ -0,0 +1,9 @@
+require File.join(File.dirname(__FILE__), 'base_kde_formula')
+
+class KdevelopPhpDocs < BaseKdeFormula
+  homepage 'http://kdevelop.org/'
+  url 'http://download.kde.org/stable/kdevelop/4.5.1/src/kdevelop-php-docs-1.5.1.tar.bz2'
+  sha1 '9bdb654373970f46d01359fdcae6d879c405760a'
+
+  kde_build_deps
+end
---
+++ kdevelop-php.rb
@@ -0,0 +1,9 @@
+require File.join(File.dirname(__FILE__), 'base_kde_formula')
+
+class KdevelopPhp < BaseKdeFormula
+  homepage 'http://kdevelop.org/'
+  url 'http://download.kde.org/stable/kdevelop/4.5.1/src/kdevelop-php-1.5.1.tar.bz2'
+  sha1 'c047724c255417b361d3ce1898555c2b9183f5b2'
+
+  kde_build_deps
+end
