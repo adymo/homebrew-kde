b60e2d379d01b850253dca2d39f4e38db525f0de
Tom Vincent <git@tlvince.com>
2013-12-14 16:13:36 +0000

    marble: preserve cmake files

--- marble.rb
+++ marble.rb
@@ -5,5 +5,10 @@ class Marble < BaseKdeFormula
   url 'http://download.kde.org/stable/4.11.4/src/marble-4.11.4.tar.xz'
   sha1 '7569630e8b2fdaafb2fdf6da79aa165ead17e842'
 
+  def patches
+    # Preserve CMake files (needed by Digikam)
+    {:p0 => 'https://gist.github.com/tlvince/7960812/raw/f96e2f2d1a681c6329da9fc9ac3dbf6c18577b0a/marble-homebrew-cmakelists.diff'}
+  end
+
   kde_build_deps
 end
