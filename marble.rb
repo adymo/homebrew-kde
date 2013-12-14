a6f9a385e1fe7c79bd4350fff59abba2996b1949
Tom Vincent <git@tlvince.com>
2013-12-14 16:15:13 +0000

    marble: Install QT app in cellar dir

--- marble.rb
+++ marble.rb
@@ -10,5 +10,10 @@ class Marble < BaseKdeFormula
     {:p0 => 'https://gist.github.com/tlvince/7960812/raw/f96e2f2d1a681c6329da9fc9ac3dbf6c18577b0a/marble-homebrew-cmakelists.diff'}
   end
 
+  def extra_cmake_args
+    # Prevent 'marble-qt.app' from being installed in '/usr/local'
+    "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+  end
+
   kde_build_deps
 end
