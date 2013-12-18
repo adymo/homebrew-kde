b2c7f6d4889d2430471142cde74c8ebe1a2226b1
Alexander Dymo <adymo@eligoenergy.com>
2013-12-18 11:57:07 -0600

    Update kdevelop-ruby to 4.6

--- kdevelop-ruby.rb
+++ kdevelop-ruby.rb
@@ -2,8 +2,8 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 
 class KdevelopRuby < BaseKdeFormula
   homepage 'http://kdevelop.org/'
-  url 'https://github.com/adymo/kdevelop-ruby/archive/kdevelop-4-5.zip'
-  sha1 '3d38391dadec9debf88629965d85924965682ffa'
+  url 'https://github.com/adymo/kdevelop-ruby/archive/kdevelop-4-6.zip'
+  sha1 'e1afd9ef104ec77528e00d1175f6e1e2ff301551'
 
   depends_on 'kdevelop'
   kde_build_deps
