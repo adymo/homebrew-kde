71d50957e05b78d6d8eeaf2e4e6104c2cefe064c
Yue Liu <yue.liu@mail.com>
2014-02-18 21:02:42 -0500

    move attica to its own keg

--- attica.rb
+++ attica.rb
@@ -1,9 +1,15 @@
-require File.join(File.dirname(__FILE__), 'base_kde_formula')
+require 'formula'
 
-class Attica < BaseKdeFormula
+class Attica < Formula
   homepage 'http://www.kde.org/'
   url 'ftp://ftp.kde.org/pub/kde/stable/attica/attica-0.4.2.tar.bz2'
   sha1 '5ae4adcb99db839902b70afd3d1cc7fd471bacd7'
+  
+  depends_on 'cmake' => :build
+  depends_on 'qt'
 
-  kde_build_deps
+  def install
+    system "cmake #{std_cmake_parameters} ."
+    system "make install"
+  end
 end
