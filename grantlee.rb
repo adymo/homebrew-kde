f1c16e178f70da74d40ce31fee3dff303b94ac0e
Alexander Dymo <adymo@eligoenergy.com>
2014-04-30 15:11:44 -0500

    sha1 for grantlee

--- grantlee.rb
+++ grantlee.rb
@@ -4,6 +4,7 @@ class Grantlee < BaseKdeFormula
   homepage 'http://grantlee.org/'
   version '0.3.0'
   url 'http://gitorious.org/grantlee/grantlee/archive-tarball/master'
+  sha1 '19763bc1dafb8aa6843660b1dd75fcc7fa67c0f4'
 
   kde_build_deps
   depends_on 'qt'
