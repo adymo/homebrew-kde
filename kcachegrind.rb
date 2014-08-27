fcc15117754b079b25479654bd120fe00474a470
Alexander Dymo <adymo@eligoenergy.com>
2014-08-27 18:06:07 -0500

    Fix class name

--- kcachegrind.rb
+++ kcachegrind.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), 'base_kde_formula')
 
-class Kactivities < BaseKdeFormula
+class Kcachegrind < BaseKdeFormula
   homepage 'http://www.kde.org/'
   url 'http://download.kde.org/stable/4.14.0/src/kcachegrind-4.14.0.tar.xz'
   sha1 '403f10048445b9348a985504ac5e80d70d563244'
