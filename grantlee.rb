09c88765315a06975639af28a3567f117407f3df
Alexander Dymo <adymo@eligoenergy.com>
2014-09-23 11:07:26 -0500

    Update grantlee sha key. Fixes #40

--- grantlee.rb
+++ grantlee.rb
@@ -4,7 +4,7 @@ class Grantlee < BaseKdeFormula
   homepage 'http://grantlee.org/'
   version '0.3.0'
   url 'http://gitorious.org/grantlee/grantlee/archive-tarball/master'
-  sha1 '19763bc1dafb8aa6843660b1dd75fcc7fa67c0f4'
+  sha1 '9f64b3d5579ae32085fa459e44581f21b08469d6'
 
   kde_build_deps
   depends_on 'qt' => ["with-d-bus", "with-qt3support"]
