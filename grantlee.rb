cea582782966eb45c2f85498706e3a7625deb78d
Ferdinand Niedermann <ferdinand.niedermann@simplificator.com>
2014-10-05 10:11:30 +0200

    Update grantlee to newest stable version (instead of master)

--- grantlee.rb
+++ grantlee.rb
@@ -2,9 +2,9 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 
 class Grantlee < BaseKdeFormula
   homepage 'http://grantlee.org/'
-  version '0.3.0'
-  url 'http://gitorious.org/grantlee/grantlee/archive-tarball/master'
-  sha1 '9f64b3d5579ae32085fa459e44581f21b08469d6'
+  version '0.5.1'
+  url 'https://gitorious.org/grantlee/grantlee/archive/v0.5.1.tar.gz'
+  sha1 '1324dc801d3a8b27fe69b73cce3dd7f91782756b'
 
   kde_build_deps
   depends_on 'qt' => ["with-d-bus", "with-qt3support"]
