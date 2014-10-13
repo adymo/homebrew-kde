2c6169bb9717c46bd521b12bfaf1db64491cf633
Jack Nagel <jacknagel@gmail.com>
2014-10-13 17:42:41 -0500

    Remove std_cmake_parameters warning
    It's unclear to me what the rationale for this warning is, but
    std_cmake_parameters is a method call and its value cannot be mutated.

--- base_kde_formula.rb
+++ base_kde_formula.rb
@@ -34,7 +34,6 @@ class BaseKdeFormula < Formula
   def extra_prefix_path
   end
   def kde_default_cmake_args
-    raise "std_cmake_parameters has changed... #{std_cmake_parameters}, prefix is #{prefix}" if std_cmake_parameters != "-DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev"
     s = extra_prefix_path
     if s.nil?
       s = kdedir
