f05864163e1d6a619a0edafacc7bd00ffa0ff04e
Alexander Dymo <alex@alexdymo.com>
2014-11-11 13:30:23 -0600

    Update sha1

--- grantlee.rb
+++ grantlee.rb
@@ -4,7 +4,7 @@ class Grantlee < BaseKdeFormula
   homepage 'http://grantlee.org/'
   version '0.5.1'
   url 'https://gitorious.org/grantlee/grantlee/archive/v0.5.1.tar.gz'
-  sha1 '1324dc801d3a8b27fe69b73cce3dd7f91782756b'
+  sha1 '4da30724c5330fe0612ea2ebcfabbe11a036c019'
 
   kde_build_deps
   depends_on 'qt' => ["with-d-bus", "with-qt3support"]
