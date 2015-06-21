f519b5a10032b8b1603cc51f611f23aa74583e8c
Jefersson Nathan <admin@phpse.net>
2015-06-21 10:51:15 -0300

    Fix unavailable server

--- attica.rb
+++ attica.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Attica < Formula
   homepage 'http://www.kde.org/'
-  url 'ftp://ftp.kde.org/pub/kde/stable/attica/attica-0.4.2.tar.bz2'
+  url 'http://download.kde.org/stable/attica/attica-0.4.2.tar.bz2'
   sha1 '5ae4adcb99db839902b70afd3d1cc7fd471bacd7'
   
   depends_on 'cmake' => :build
