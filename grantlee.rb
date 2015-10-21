033ae15e38bf43b0e0b6207041213b198e09272d
Alexander Dymo <adymo@eligoenergy.com>
2015-10-21 17:31:00 -0500

    A better source of grantlee tarball, I hope

--- grantlee.rb
+++ grantlee.rb
@@ -3,7 +3,8 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 class Grantlee < BaseKdeFormula
   homepage 'http://grantlee.org/'
   version '0.5.1'
-  url 'https://gitorious.org/grantlee/grantlee.git', :using => :git, :tag => "v0.5.1"
+  url 'http://www.grantlee.org/downloads/grantlee-0.5.1.tar.gz'
+  sha1 'fe7111bcf25d1e3ead8e48f0f1aea91df944e58d'
   head 'https://gitorious.org/grantlee/grantlee.git'
 
   kde_build_deps
