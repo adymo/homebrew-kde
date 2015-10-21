5918a171aedde4ae270466ea9dc75fc873b119eb
Alexander Dymo <adymo@eligoenergy.com>
2015-10-21 18:30:47 -0500

    Update kdevelop-ruby sha

--- kdevelop-ruby.rb
+++ kdevelop-ruby.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 class KdevelopRuby < BaseKdeFormula
   homepage 'http://kdevelop.org/'
   url 'https://github.com/adymo/kdevelop-ruby/archive/kdevelop-4-7.zip'
-  sha1 'a25bcdd0b6f6db82643f9e1b61524aab106cf065'
+  sha1 'ffaf1e16de9744f4e6da710622fc970f5b746ba9'
 
   depends_on 'kdevelop'
   kde_build_deps
